9a70f80fc621c97409b2bd1bca6afa0c66c856a6
Kirilo Lozitsky <bondxxl01@gmail.com>
2020-03-08 00:34:10 +0200

    Added effects with transitions

--- src/com.kirilo.java.javafx/Main.java
+++ src/com.kirilo.java.javafx/Main.java
@@ -1,13 +1,15 @@
 package com.kirilo.java.javafx;
 
+import javafx.animation.*;
 import javafx.application.Application;
+import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.text.Font;
-import javafx.scene.text.Text;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
+//https://docs.oracle.com/javafx/2/scenegraph/jfxpub-scenegraph.htm
 public class Main extends Application {
     public static void main(String[] args) {
         launch(args);
@@ -15,17 +17,37 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Button button = new Button("test");
-        Text text = new Text(10, 20, "Test JavaFX");
-        text.setFont(new Font(30));
 
-        BorderPane pane = new BorderPane();
-        pane.setCenter(button);
-        pane.setTop(text);
+        Group root = new Group();
 
-        Scene scene = new Scene(pane, 400, 400);
+        Scene scene = new Scene(root, 500, 500);
 
-        primaryStage.setTitle("JavaFX title");
+        Rectangle r = new Rectangle(0, 0, 250, 250);
+        r.setFill(Color.BLUE);
+        root.getChildren().add(r);
+
+        TranslateTransition translate =
+                new TranslateTransition(Duration.millis(750));
+        translate.setToX(390);
+        translate.setToY(390);
+
+        FillTransition fill = new FillTransition(Duration.millis(750));
+        fill.setToValue(Color.RED);
+
+        RotateTransition rotate = new RotateTransition(Duration.millis(750));
+        rotate.setToAngle(360);
+
+        ScaleTransition scale = new ScaleTransition(Duration.millis(750));
+        scale.setToX(0.1);
+        scale.setToY(0.1);
+
+        ParallelTransition transition = new ParallelTransition(r,
+                translate, fill, rotate, scale);
+        transition.setCycleCount(Timeline.INDEFINITE);
+        transition.setAutoReverse(true);
+        transition.play();
+
+        primaryStage.setTitle("JavaFX Scene Graph Demo");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
